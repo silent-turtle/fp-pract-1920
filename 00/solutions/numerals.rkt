e7923a270566a665954028c550f0cbb9ac89e2f7
silent-turtle <tkamenov8@gmail.com>
2019-11-07 00:30:30 +0200

    solution to task 3 added

--- 00/solutions/numerals.rkt
+++ 00/solutions/numerals.rkt
@@ -7,17 +7,41 @@
          pred)
 
 (define zero (lambda (f v) v))
+(define one (lambda (f v) (f v)))
+(define two (lambda (f v) (f (f v))))
 
 (define (succ n)
   (lambda (f v)
     (f (n f v))))
 
-(define (from-numeral n) void)
+(define (from-numeral n)
+  (define num 0)
+  (define fun (lambda (x) (+ x 1)))
+  (define (helper current cnt)
+    (if (= (current fun num) (n fun num))
+        cnt
+        (helper (succ current) (+ cnt 1))))
+  (helper zero 0))
+    
 
-(define (to-numeral n) void)
+(define (to-numeral n)
+  (define (helper res current)
+    (if (= current n)
+        res
+        (helper (succ res) (+ current 1))))
+  (helper zero 0))
+  
 
-(define (plus n m) void)
+(define (plus n m)
+  (lambda (f v) (m f (n f v))))
 
-(define (mult n m) void)
+(define (mult n m)
+  (lambda (f v)
+    (define (helper i res)
+      (if (= (i f v) (n f v))
+          (res f v)
+          (helper (succ i) (plus res m))))
+    (helper zero zero)))
+    
 
 (define (pred n) void)
