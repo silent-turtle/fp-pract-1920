2ab69fa9ee53c0b697daf4a6442c91749730f5fd
silent-turtle <tkamenov8@gmail.com>
2019-11-06 12:35:00 +0200

    solution to first task added

--- 00/solutions/k-ary.rkt
+++ 00/solutions/k-ary.rkt
@@ -3,6 +3,16 @@
 (provide from-k-ary
          to-k-ary)
 
-(define (from-k-ary n k) void)
+(define (from-k-ary n k)
+  (define (helper res n cnt)
+    (if (= n 0)
+        res
+        (helper (+ (* (expt k cnt) (remainder n 10)) res) (quotient n 10) (+ cnt 1))))
+  (helper 0 n 0))
 
-(define (to-k-ary n k) void)
+(define (to-k-ary n k)
+  (define (helper res n cnt)
+    (if (< n k)
+        (+ (* (expt 10 cnt) n) res)
+        (helper (+  (* (expt 10 cnt) (remainder n k)) res) (quotient n k) (+ cnt 1))))
+  (helper 0 n 0))
