62b9a8aa2fcda327636fdcb96899012004daa165
silent-turtle <tkamenov8@gmail.com>
2019-12-19 16:40:24 +0200

    fixed syntax

--- 02/src/Stuff.hs
+++ 02/src/Stuff.hs
@@ -11,7 +11,7 @@ module Stuff
 
 group :: Eq a => [a] -> [[a]]
 group [] = []
-group (x:xs) = ((x : takeWhile (==x) xs) : group (dropWhile (==x) xs))
+group (x:xs) = (x : takeWhile (==x) xs) : group (dropWhile (==x) xs)
 
 sortBy :: (a -> a -> Ordering) -> [a] -> [a]
 sortBy _ [] = []
@@ -19,35 +19,35 @@ sortBy comp list = mergeSort list
   where merge [] xs = xs
         merge xs [] = xs
         merge (x:xs) (y:ys)
-          | ((comp x y) == LT) || ((comp x y ) == EQ) = (x : merge xs (y:ys))
-          | otherwise = (y : merge (x:xs) ys)
+          | (comp x y == LT) || (comp x y == EQ) = x : merge xs (y:ys)
+          | otherwise = y : merge (x:xs) ys
         split xs n = splitHelper xs n []
           where splitHelper ys m buffer
-                  | m == 0 || (null ys) = (reverse buffer, ys)
-                  | otherwise = splitHelper (tail ys) (pred m) ((head ys):buffer)
+                  | m == 0 || null ys = (reverse buffer, ys)
+                  | otherwise = splitHelper (tail ys) (pred m) (head ys : buffer)
         mergeSort [] = []
         mergeSort [x] = [x]
         mergeSort xs = merge (mergeSort (fst splitxs)) (mergeSort (snd splitxs))
-          where splitxs = split xs ((length xs) `quot`  2)
+          where splitxs = split xs (length xs `quot`  2)
 
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
 groupBy _ [] = []
-groupBy p (x:xs) = ((x:takeWhile (p x) xs) : groupBy p (dropWhile (p x) xs))
+groupBy p (x:xs) = (x:takeWhile (p x) xs) : groupBy p (dropWhile (p x) xs)
 
 on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-on f g x y = (f (g x) (g y))
+on f g x y = f (g x) (g y)
 
 (&&&) :: (a -> b) -> (a -> c) -> a -> (b, c)
-(&&&) f g x = (,) (f x) (g x)
+(&&&) f g x = (f x, g x)
 
 sortOn :: Ord b => (a -> b) -> [a] -> [a]
 sortOn _ [] = []
-sortOn f xs = map fst (sortBy (compare `on` snd) (map ((&&&) id f) xs)) 
+sortOn f xs = map fst (sortBy (compare `on` snd) (map (id &&& f) xs)) 
 
 groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
 groupOn _ [] = []
-groupOn f xs = map (map fst) (groupBy ((==) `on` snd) (map ((&&&) id f) xs))
+groupOn f xs = map (map fst) (groupBy ((==) `on` snd) (map (id &&& f) xs))
 
 classifyOn :: Ord b => (a -> b) -> [a] -> [[a]]
 classifyOn _ [] = []
-classifyOn f xs = map (map fst) (groupBy ((==) `on` snd) (sortBy (compare `on` snd) (map ((&&&) id f) xs))) 
+classifyOn f xs = map (map fst) (groupBy ((==) `on` snd) (sortBy (compare `on` snd) (map (id &&& f) xs))) 
